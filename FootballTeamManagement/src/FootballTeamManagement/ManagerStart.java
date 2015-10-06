808efb854f3e1ffd2e7344904e60c371f26b9fe1
jayakrishnancn <jayakrishnancn@gmail.com>
2015-10-06 20:40:49 +0530

    update1

---
+++ FootballTeamManagement/src/FootballTeamManagement/ManagerStart.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FootballTeamManagement;
+
+/**
+ *
+ * @author jayakrishnan
+ */
+public class ManagerStart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Login().setVisible(true);
+    }
+}
