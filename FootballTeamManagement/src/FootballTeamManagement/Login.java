d152de2b1a4ca7593c67bc675f4554454996ae06
jayakrishnancn <jayakrishnancn@gmail.com>
2015-10-09 20:12:25 +0530

    Rollback tests

--- FootballTeamManagement/src/FootballTeamManagement/Login.java
+++ FootballTeamManagement/src/FootballTeamManagement/Login.java
@@ -20,9 +20,7 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         this.setTitle("Log In ");
-        this.setIconImage(new ImageIcon(getClass().getResource("logo.jpg")).getImage());
-        System.out.println("this is for AjayKrishnan");
-        System.out.println("this is for AjayKrishnan 2");
+        this.setIconImage(new ImageIcon(getClass().getResource("logo.jpg")).getImage()); 
         
     }
 
